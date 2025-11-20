b8ec882a01d8e71a7274f46b87fc5792659e0bdd
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-11-20 12:37:31 +0600

    deployment test

--- helpers/constants/pageConst.tsx
+++ helpers/constants/pageConst.tsx
@@ -1,6 +1,6 @@
 //hero section const
 export const NAME = "Anik Saha";
-export const TITLE = "Software Engineer | Full Stack Developer";
+export const TITLE = "Software Associate | Full Stack Developer";
 export const DESCRIPTION =
   "I specialize in creating modern web applications using React, Node.js, and Laravel. I love building scalable systems and solving real-world problems through technology.";
 export const IMAGE_SRC = "/images/hero/Anik.jpeg";
