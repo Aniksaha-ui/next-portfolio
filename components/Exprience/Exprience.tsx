c41557ff6acb37e1e6184a05f61f38be811029ec
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-11-23 10:18:23 +0600

    experience added

--- components/Exprience/Exprience.tsx
+++ components/Exprience/Exprience.tsx
@@ -95,7 +95,7 @@ const Experience = () => {
       className="bg-gray-light dark:bg-bg-color-dark py-16 md:py-20 lg:py-28"
     >
       <div className="container">
-        <SectionTitle title={BLOGS} paragraph={BLOGS_DESCRIPTION} center />
+        <SectionTitle title={EXPERIENCE} paragraph={EXPERIENCE_DESCRIPTION} center />
 
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
           {experience &&
